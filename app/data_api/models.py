4f807bea8fdd8c40d54e506d415beeec883bb67f
henbk <henrique.kubenda@gmail.com>
2024-01-06 11:19:31 -0300

    add custom user model

--- app/data_api/models.py
+++ app/data_api/models.py
@@ -1,3 +1,49 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    """Custom manager for user profiles"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email address')
+
+        # Normalize email address
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        # Set password
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create a new superuser profile"""
+        user = self.create_user(email, name, password)
+
+        # Set user to superuser
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
